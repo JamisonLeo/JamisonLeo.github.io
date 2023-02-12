87bc144f69aa2a433d4e8812f81738cae85976d4
LiuJunxian <12303668+MisterJunxian@user.noreply.gitee.com>
2023-02-12 22:55:16 +0800

    优化手机端界面

---
+++ js/index_mobile.js
@@ -0,0 +1,43 @@
+function changeVideo() {
+    document.getElementById("video").src = "../images/video1.png";
+    document.getElementById("text1").style.color = "rgb(125, 197, 235)";
+    document.getElementById("picture").src = "../images/picture.png";
+    document.getElementById("text2").style.color = "rgb(191, 191, 191)";
+    document.getElementById("code").src = "../images/code.png";
+    document.getElementById("text3").style.color = "rgb(191, 191, 191)";
+    document.getElementById("directory").src = "../images/directory.png";
+    document.getElementById("text4").style.color = "rgb(191, 191, 191)";
+}
+
+function changePicture() {
+    document.getElementById("video").src = "../images/video.png";
+    document.getElementById("text1").style.color = "rgb(191, 191, 191)";
+    document.getElementById("picture").src = "../images/picture1.png";
+    document.getElementById("text2").style.color = "rgb(125, 197, 235)";
+    document.getElementById("code").src = "../images/code.png";
+    document.getElementById("text3").style.color = "rgb(191, 191, 191)";
+    document.getElementById("directory").src = "../images/directory.png";
+    document.getElementById("text4").style.color = "rgb(191, 191, 191)";
+}
+
+function changeCode() {
+    document.getElementById("video").src = "../images/video.png";
+    document.getElementById("text1").style.color = "rgb(191, 191, 191)";
+    document.getElementById("picture").src = "../images/picture.png";
+    document.getElementById("text2").style.color = "rgb(191, 191, 191)";
+    document.getElementById("code").src = "../images/code1.png";
+    document.getElementById("text3").style.color = "rgb(125, 197, 235)";
+    document.getElementById("directory").src = "../images/directory.png";
+    document.getElementById("text4").style.color = "rgb(191, 191, 191)";
+}
+
+function changeDirectory() {
+    document.getElementById("video").src = "../images/video.png";
+    document.getElementById("text1").style.color = "rgb(191, 191, 191)";
+    document.getElementById("picture").src = "../images/picture.png";
+    document.getElementById("text2").style.color = "rgb(191, 191, 191)";
+    document.getElementById("code").src = "../images/code.png";
+    document.getElementById("text3").style.color = "rgb(191, 191, 191)";
+    document.getElementById("directory").src = "../images/directory1.png";
+    document.getElementById("text4").style.color = "rgb(125, 197, 235)";
+}
