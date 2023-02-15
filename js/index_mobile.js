7b7e25f4bf2ec0e98cad74a2a7a30e0ab09ac1bc
LiuJunxian <12303668+MisterJunxian@user.noreply.gitee.com>
2023-02-15 14:36:26 +0800

    调整手机端界面及底部导航

--- js/index_mobile.js
+++ js/index_mobile.js
@@ -1,43 +1,51 @@
 function changeVideo() {
-    document.getElementById("video").src = "../images/video1.png";
+    document.getElementById("video").setAttribute("style", "fill: rgb(125, 197, 235)");
     document.getElementById("text1").style.color = "rgb(125, 197, 235)";
-    document.getElementById("picture").src = "../images/picture.png";
+    document.getElementById("picture1").setAttribute("style", "fill: rgb(191, 191, 191)");
+    document.getElementById("picture2").setAttribute("style", "fill: rgb(191, 191, 191)");
     document.getElementById("text2").style.color = "rgb(191, 191, 191)";
-    document.getElementById("code").src = "../images/code.png";
+    document.getElementById("code1").setAttribute("style", "fill: rgb(191, 191, 191)");
+    document.getElementById("code2").setAttribute("style", "fill: rgb(191, 191, 191)");
     document.getElementById("text3").style.color = "rgb(191, 191, 191)";
-    document.getElementById("directory").src = "../images/directory.png";
+    document.getElementById("directory").setAttribute("style", "fill: rgb(191, 191, 191)");
     document.getElementById("text4").style.color = "rgb(191, 191, 191)";
 }
 
 function changePicture() {
-    document.getElementById("video").src = "../images/video.png";
+    document.getElementById("video").setAttribute("style", "fill: rgb(191, 191, 191)");
     document.getElementById("text1").style.color = "rgb(191, 191, 191)";
-    document.getElementById("picture").src = "../images/picture1.png";
+    document.getElementById("picture1").setAttribute("style", "fill: rgb(125, 197, 235)");
+    document.getElementById("picture2").setAttribute("style", "fill: rgb(125, 197, 235)");
     document.getElementById("text2").style.color = "rgb(125, 197, 235)";
-    document.getElementById("code").src = "../images/code.png";
+    document.getElementById("code1").setAttribute("style", "fill: rgb(191, 191, 191)");
+    document.getElementById("code2").setAttribute("style", "fill: rgb(191, 191, 191)");
     document.getElementById("text3").style.color = "rgb(191, 191, 191)";
-    document.getElementById("directory").src = "../images/directory.png";
+    document.getElementById("directory").setAttribute("style", "fill: rgb(191, 191, 191)");
     document.getElementById("text4").style.color = "rgb(191, 191, 191)";
 }
 
 function changeCode() {
-    document.getElementById("video").src = "../images/video.png";
+    document.getElementById("video").setAttribute("style", "fill: rgb(191, 191, 191)");
     document.getElementById("text1").style.color = "rgb(191, 191, 191)";
-    document.getElementById("picture").src = "../images/picture.png";
+    document.getElementById("picture1").setAttribute("style", "fill: rgb(191, 191, 191)");
+    document.getElementById("picture2").setAttribute("style", "fill: rgb(191, 191, 191)");
     document.getElementById("text2").style.color = "rgb(191, 191, 191)";
-    document.getElementById("code").src = "../images/code1.png";
+    document.getElementById("code1").setAttribute("style", "fill: rgb(125, 197, 235)");
+    document.getElementById("code2").setAttribute("style", "fill: rgb(125, 197, 235)");
     document.getElementById("text3").style.color = "rgb(125, 197, 235)";
-    document.getElementById("directory").src = "../images/directory.png";
+    document.getElementById("directory").setAttribute("style", "fill: rgb(191, 191, 191)");
     document.getElementById("text4").style.color = "rgb(191, 191, 191)";
 }
 
 function changeDirectory() {
-    document.getElementById("video").src = "../images/video.png";
+    document.getElementById("video").setAttribute("style", "fill: rgb(205, 205, 205)");
     document.getElementById("text1").style.color = "rgb(191, 191, 191)";
-    document.getElementById("picture").src = "../images/picture.png";
+    document.getElementById("picture1").setAttribute("style", "fill: rgb(191, 191, 191)");
+    document.getElementById("picture2").setAttribute("style", "fill: rgb(191, 191, 191)");
     document.getElementById("text2").style.color = "rgb(191, 191, 191)";
-    document.getElementById("code").src = "../images/code.png";
+    document.getElementById("code1").setAttribute("style", "fill: rgb(191, 191, 191)");
+    document.getElementById("code2").setAttribute("style", "fill: rgb(191, 191, 191)");
     document.getElementById("text3").style.color = "rgb(191, 191, 191)";
-    document.getElementById("directory").src = "../images/directory1.png";
+    document.getElementById("directory").setAttribute("style", "fill: rgb(125, 197, 235)");
     document.getElementById("text4").style.color = "rgb(125, 197, 235)";
 }
