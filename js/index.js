c033f3d6d7544e3733e2cca8a4af809e92404bfe
LiuJunxian <12303668+MisterJunxian@user.noreply.gitee.com>
2023-02-15 15:19:16 +0800

    密码使用MD5加密，密码错误自动清空输入框

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 function enter() {
     var input = pwdInput.password;
-    var pwd = 666666;
     var result = document.getElementById("result");
-    if (input.value == pwd) {
+    if (hex_md5(input.value) == "f379eaf3c831b04de153469d1bec345e") {
         location.replace("html/shouye.html");
     } else {
+        input.value = "";
         result.innerText = "密码错误！\n请联系管理员：liujunxian1025@qq.com";
         result.style.display = "block";
     }
